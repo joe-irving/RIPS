b9eabfd7a7b8fdcfac0eed146e5367b4f0eb47ec
joe-irving <joeirving.jbi@gmail.com>
2023-11-14 12:24:13 +0000

    update to free map tile layer

--- assets/js/map.js
+++ assets/js/map.js
@@ -7,9 +7,9 @@ var prisonMap = L.map("prison-map",{
                 });
 
 var titleLayer = L.tileLayer(
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}",
+  "https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
   {
-    "attribution": '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    "attribution": '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     "detectRetina": false,
     "maxNativeZoom": 20,
     "maxZoom": 20,
